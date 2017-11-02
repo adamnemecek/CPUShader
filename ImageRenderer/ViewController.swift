8e6641e2c923d0cacbfdd12014dff16517d4ab20
Adam Nemecek <adamnemecek@gmail.com>
2017-11-02 07:38:40 -0700

    Initial Commit

---
+++ ImageRenderer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageRenderer
+//
+//  Created by Adam Nemecek on 11/2/17.
+//  Copyright © 2017 Adam Nemecek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ImageRendererTests/ImageRendererTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageRendererTests.swift
+//  ImageRendererTests
+//
+//  Created by Adam Nemecek on 11/2/17.
+//  Copyright © 2017 Adam Nemecek. All rights reserved.
+//
+
+import XCTest
+@testable import ImageRenderer
+
+class ImageRendererTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
